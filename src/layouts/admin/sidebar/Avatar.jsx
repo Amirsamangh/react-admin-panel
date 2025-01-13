8cfa0f31a64b8d192547a9c3c47a843d46b037b3
AmirSaman Gharechahi <gharechahi72@gmail.com>
2025-01-13 14:38:27 +0330

    react componrnts 94

---
+++ src/layouts/admin/sidebar/Avatar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Avatar = ({name , imagePath}) => {
+    return (
+            <div className="pt-1 pb-2 mb-4 d-flex flex-column avatar_li position-relative">
+                <span className="avatar_box">
+                    <img
+                        className="w-100 rounded-circle"
+                        src={imagePath}
+                        alt="user"
+                    />
+                </span>
+                <div className="sidebar_avatar_name text-center hiddenable">
+                    {name}
+                </div>
+            </div>
+    );
+}
+
+export default Avatar;
