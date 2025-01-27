d2366792102af18d33331ca83c438110ea78856a
AmirSaman Gharechahi <gharechahi72@gmail.com>
2025-01-28 01:57:54 +0330

    react authentication 107

--- src/layouts/auth/AuthLayout.jsx
+++ src/layouts/auth/AuthLayout.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from '../../pages/auth/Login';
+import { useIsLogin } from '../../hook/authhook';
 
 const AuthLayout = () => {
+
+    const [loading, isLogin] = useIsLogin();
+
     return (
+
+
         <div className='limiter'>
-            <div className='container-login100'>
-                <Routes>
-                    <Route path='/login' element={<Login/>}/>
-                </Routes>
-            </div>
+            {
+                loading ? (
+                    <h1 className="text-center waiting_center">
+                        لطفا صبر کنید
+                    </h1>
+                ) : !isLogin ? (
+                    <div>
+                        <Routes>
+                            <Route path='/auth/login' element={<Login />} />
+                        </Routes>
+                    </div>
+                ) : (
+                    <Navigate to={'/'} />
+                )
+            }
         </div>
+
+
+        // <div className='limiter'>
+        //     <div className='container-login100'>
+
+        //     </div>
+        // </div>
     );
 }
 
