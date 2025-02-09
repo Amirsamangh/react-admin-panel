9bba7c968d94391b6d116b9c4bed6e99041af4c0
AmirSaman Gharechahi <gharechahi72@gmail.com>
2025-02-09 19:49:03 +0330

    react project - add attributes 121

--- src/pages/category/attrs/AddAttrbute.jsx
+++ src/pages/category/attrs/AddAttrbute.jsx
@@ -4,8 +4,12 @@ import PaginatedTable from '../../../components/PaginatedTable';
 import ShowInFilter from './ShowInFilter';
 import AttrActions from './AttrActions';
 import PrevPageButton from '../../../components/PrevPageButton';
-import { getCategoryAttrService } from '../../../services/categoryAttr';
+import { addCategoryAttrService, getCategoryAttrService } from '../../../services/categoryAttr';
 import { Alert } from '../../../utils/alerts';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import FormikControl from '../../../components/form/FormikControl';
+import SubmitButton from '../../../components/form/submitButton';
 
 const AddAttrbute = () => {
 
@@ -15,8 +19,8 @@ const AddAttrbute = () => {
 
     const dataInfo = [
         { field: 'id', title: '#' },
-        { field: 'title', title: 'عنوان محصول' },
         { field: 'unit', title: 'واحد' },
+        { field: 'title', title: 'عنوان محصول' },
     ]
     const additionalFieald = [
         {
@@ -26,7 +30,7 @@ const AddAttrbute = () => {
         {
             title: 'عملیات',
             elements: (rowData) => <AttrActions rowData={rowData} />
-        }
+        },
     ]
     const searchParams = {
         title: 'جستجو',
@@ -34,20 +38,19 @@ const AddAttrbute = () => {
         searchField: 'title',
     }
 
-    const handleGetCategoryAttr = async ()=>{
+    const handleGetCategoryAttr = async () => {
         setLoading(true);
         try {
             const res = await getCategoryAttrService(location.state.categoryData.id)
             console.log(res);
-            
-            if (res.status == 200) {
+
+            if (res.status === 200) {
                 setData(res.data.data)
-                Alert( '' , res.data.message)
             }
         } catch (error) {
             console.log(error);
-            Alert('' , error.message)
-            
+            Alert('', error.message)
+
         } finally {
             setLoading(false);
         }
@@ -55,35 +58,86 @@ const AddAttrbute = () => {
     useEffect(() => {
         handleGetCategoryAttr();
     }, [])
+    
+
+    const initialValues = {
+        title: '',
+        unit: '',
+        in_filter: true
+    }
+
+    const onSubmit = async (values, actions , catId , setData ) => {
+        try {
+            values = {
+                ...values ,
+                in_filter: values.in_filter ? 1 : 0
+            }
+            const res = await addCategoryAttrService(catId , values)
+            if(res.status === 201) {
+                Alert('انجام شد' , res.data.message , 'success')
+                setData(oldData=>[...oldData , res.data.data])
+            }
+        } catch (error) {
+            console.log(error.message);
+            
+        } finally {
+            actions.setSubmitting(false)
+        }
+    }
+
+    const validationSchema = Yup.object({
+        title: Yup.string().required('لطفا این قسمت را پر کنید').matches(/^[u0600-\u06FF\sa-zA-Z0-9!@$%&?]+$/, 'فقط از حروف و اعداد استفاده شود'),
+        unit: Yup.string().required('لطفا این قسمت را پر کنید').matches(/^[u0600-\u06FF]/, 'فقط از حروف استفاده شود'),
+        in_filter: Yup.boolean(),
+    })
     return (
         <>
             <div className="container">
-                <div className="row justify-content-center">
+                <div className="row d-flex justify-content-around align-items-center">
                     <div className="row my-3">
                         <div className='d-flex flex-col justify-content-center'>
                             <h3>مدیریت ویژگی ها</h3>
                             <h5 className='text-danger'>{location.state.categoryData.title}</h5>
                         </div>
-
                     </div>
-                    <div className="row my-3">
-                        <div className="col-12 col-md-6 col-lg-4 my-1">
-                            <input type="text" className="form-control" placeholder="عنوان ویژگی جدید" />
-                        </div>
-                        <div className="col-12 col-md-6 col-lg-4 my-1">
-                            <input type="text" className="form-control" placeholder="واحد ویژگی جدید" />
-                        </div>
-                        <div className="col-8 col-lg-2 my-1">
-                            <div className="form-check form-switch d-flex justify-content-center align-items-center p-0 h-100">
-                                <label className="form-check-label pointer" htmlFor="flexSwitchCheckDefault">نمایش در فیلتر</label>
-                                <input className="form-check-input pointer mx-3" type="checkbox" id="flexSwitchCheckDefault" />
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={(values, actions) => { onSubmit(values, actions , location.state.categoryData.id , setData , setLoading) }}
+                        validationSchema={validationSchema}
+                    >
+                        <Form>
+
+
+                            <div className="row my-3">
+                                <FormikControl
+                                    control='input'
+                                    className='col-md-6 col-lg-4 my-1'
+                                    type='text'
+                                    name='title'
+                                    label='عنوان'
+                                    placeholder='عنوان ویژگی جدید'
+                                />
+                                <FormikControl
+                                    control='input'
+                                    className='col-md-6 col-lg-4 my-1'
+                                    type='text'
+                                    name='unit'
+                                    label='واحد'
+                                    placeholder='واحد ویژگی جدید'
+                                />
+                                <div className="col-8 col-lg-2 my-1 ">
+                                    <FormikControl
+                                        control='switch'
+                                        name='in_filter'
+                                        label='نمایش در فیلتر'
+                                    />
+                                </div>
+                                <div className="col-4 col-lg-2 d-flex justify-content-center align-items-start my-1">
+                                    <SubmitButton />
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-4 col-lg-2 d-flex justify-content-center align-items-center my-1">
-                            <i className="fas fa-check text-light bg-success rounded-circle p-2 mx-1 hoverable_text hoverable pointer has_tooltip hoverable_text" title="ثبت ویژگی" data-bs-toggle="tooltip" data-bs-placement="top"></i>
-                        </div>
-
-                    </div>
+                        </Form>
+                    </Formik>
                     <hr />
                     <PaginatedTable
                         data={data}
