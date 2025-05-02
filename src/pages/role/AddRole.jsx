c5c467aa73e6334146bf777361b4d2129ca2207f
AmirSaman Gharechahi <gharechahi72@gmail.com>
2025-05-02 13:20:41 +0330

    react project - roles 148

--- src/pages/role/AddRole.jsx
+++ src/pages/role/AddRole.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ModalsContainer from '../../components/ModalsContainer';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useOutletContext } from 'react-router-dom';
 import { Form, Formik } from 'formik';
 import FormikControl from '../../components/form/FormikControl';
 import { initialValues, onSubmit, validationSchema } from './core';
 import SubmitButton from '../../components/form/SubmitButton';
+import { getAllPermissionsService } from '../../services/users';
 
 const AddRole = () => {
 
     const navigate = useNavigate()
     const location = useLocation()
     const roleToEdit = location.state?.roleToEdit
+    const { setData } = useOutletContext()
+
+
+    const [permissions, setPermissions] = useState([])
+
+    const handleGetAllPermissions = async () => {
+        const res = await getAllPermissionsService()
+        if (res.status === 200) {
+            setPermissions(res.data.data.map(p => { return { id: p.id, title: p.title } }))
+        }
+    }
+
+    useEffect(() => {
+        handleGetAllPermissions()
+    }, [])
 
 
     return (
@@ -19,15 +35,15 @@ const AddRole = () => {
                 className={'show d-block'}
                 fullScreen={true}
                 id='add_role_modal'
-                title= {roleToEdit ? 'ویرایش نقش' : 'افزودن نقش'}
+                title={roleToEdit ? 'ویرایش نقش' : 'افزودن نقش'}
                 closeFunction={() => { navigate(-1) }}
             >
                 <div className="container">
 
                     <Formik
-                        onSubmit={(values, actions) => onSubmit(values, actions, roleToEdit)}
-                        validationSchema={validationSchema}
                         initialValues={initialValues}
+                        onSubmit={(values, actions) => onSubmit(values, actions, setData)}
+                        validationSchema={validationSchema}
                     >
                         <Form className='row justify-content-center'>
                             <FormikControl
@@ -46,6 +62,14 @@ const AddRole = () => {
                                 label='توضیحات نقش'
                                 placeholder='فقط از حروف فارسی و لاتین استفاده کنید'
                             />
+                            <FormikControl
+                                className='col-md-8'
+                                control='checkBox'
+                                name='permissions_id'
+                                label='دسترسی ها'
+                                options={permissions}
+                            />
+
                             <div className="btn_box text-center col-12 mt-4">
                                 <SubmitButton />
                             </div>
--- src/pages/role/core.js
+++ src/pages/role/core.js
@@ -2,20 +2,24 @@ import * as Yup from 'yup'
 import { addNewRoleService } from '../../services/users'
 import { Alert } from '../../utils/alerts'
 
-export const initialValues = {}
+export const initialValues = {
+    title: '',
+    description: '',
+    permissions_id: [],
+}
 
-export const validationSchema = Yup.object({
+export const onSubmit = async (values, actions, setData) => {
 
-})
-
-export const onSubmit = (values , actions , roleToEdit) =>{
-    if(roleToEdit) {
-
-    } else {
-        const res = addNewRoleService(values)
-        if(res.status === 201) {
-            Alert('موفق' , res.data.message , 'success')
-            actions.resetForm()
-        }
+    const res = await addNewRoleService(values)
+    if (res.status === 201) {
+        Alert('موفق', res.data.message, 'success')
+        actions.resetForm()
+        setData(old => [...old, res.data.data])
     }
 }
+
+export const validationSchema = Yup.object().shape({
+    title: Yup.string().required('لطفا این قسمت را پر کنید').matches(/^[\u0600-\u06FF\sa-zA-Z0-9@!%-.$?&]+$/ , 'فقط از حروف و اعداد استفاده شود'),
+    description: Yup.string().required('لطفا این قسمت را پر کنید').matches(/^[\u0600-\u06FF\sa-zA-Z0-9@!%-.$?&]+$/ , 'فقط از حروف و اعداد استفاده شود'),
+    permissions_id: Yup.array().min(1 , 'حداقل یک مورد را انتخاب کنید'),
+})
