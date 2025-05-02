c28c75e5c30844c192b4fb72cab19803019081ec
Amirsaman <amirsaman_gh@live.com>
2025-05-02 13:20:41 +0330

    react project - roles 148

--- src/components/form/FormikControl.jsx
+++ src/components/form/FormikControl.jsx
@@ -8,6 +8,7 @@ import MultiSelect from './MultiSelect';
 import SearchableSelect from './SearchableSelect';
 import Ckeditor from './Ckeditor';
 import Date from './Date';
+import CheckBox from './CheckBox';
 
 const FormikControl = (props) => {
     switch (props.control) {
@@ -37,6 +38,9 @@ const FormikControl = (props) => {
             
         case 'date':
             return <Date {...props} />
+        
+        case 'checkBox':
+            return <CheckBox {...props} />
 
         default:
             return null
