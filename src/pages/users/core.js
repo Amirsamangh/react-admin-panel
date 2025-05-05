6dd69934eafb4721740066afa8b5acdaf8dbf683
Amirsaman <amirsaman_gh@live.com>
2025-05-05 12:26:38 +0330

    react project - add user 152

--- src/pages/users/core.js
+++ src/pages/users/core.js
@@ -1,4 +1,9 @@
 import * as Yup from 'yup'
+import { addNewUserService, editUserSevice } from '../../services/users'
+import { convertDateToJalali } from '../../utils/convertDate'
+import { Alert } from '../../utils/alerts'
+import { useNavigate } from 'react-router-dom'
+
 
 export const initialValues = {
     user_name: '',
@@ -13,17 +18,31 @@ export const initialValues = {
     roles_id: [],
 }
 
-export const onSubmit = (values, actions , setData , userIdToEdit) => {}
+export const onSubmit = async (values, actions, setData, userId , navigate) => {
+    values = { ...values, birth_date: values.birth_date ? convertDateToJalali(values.birth_date, 'jD / jM / jYYYY') : null }
+
+    if (userId) {
+
+    } else {
+        const res = await addNewUserService(values)
+        if (res.status === 201) {
+            Alert('انجام شد' , res.data.message , 'success')
+            actions.resetForm()
+            setData(old=>[...old , res.data.data])
+            navigate('/users')
+        }
+    }
+}
 
 export const validationSchema = Yup.object().shape({
-    user_name: Yup.string().required('لطفا این قسمت را پر کنید').matches(/^[u0600-\u06FF\sa-zA-Z0-9@!%&-.?]+$/ , 'فقط از حروف فارسی و لاتین و اعداد استفاده شود'),
-    first_name: Yup.string().matches(/^[u0600-\u06FF\sa-zA-Z]+$/ , 'فقط از حروف فارسی و لاتین استفاده شود'),
-    last_name: Yup.string().matches(/^[u0600-\u06FF\sa-zA-Z]+$/ , 'فقط از حروف فارسی و لاتین استفاده شود'),
-    phone: Yup.number().required('لطفا این قسمت را پر کنید'),
-    national_code: Yup.number().required('لطفا این قسمت را پر کنید'),
+    user_name: Yup.string().required('لطفا این قسمت را پر کنید').matches(/^[u0600-\u06FF\sa-zA-Z0-9@!%&-.?]+$/, 'فقط از حروف فارسی و لاتین و اعداد استفاده شود'),
+    first_name: Yup.string().matches(/^[u0600-\u06FF\sa-zA-Z]+$/, 'فقط از حروف فارسی و لاتین استفاده شود'),
+    last_name: Yup.string().matches(/^[u0600-\u06FF\sa-zA-Z]+$/, 'فقط از حروف فارسی و لاتین استفاده شود'),
+    phone: Yup.number().typeError('لطفا فقط از عدد استفاده کنید').required('لطفا این قسمت را پر کنید'),
+    national_code: Yup.number().typeError('لطفا کد ملی خود را به درستی وارد کنید').required('لطفا این قسمت را پر کنید'),
     email: Yup.string().email('لطفا از فرمت صحیح ایمیل استفاده کنید'),
-    password: Yup.string().required('لطفا این قسمت را پر کنید').matches(/^[u0600-\u06FF\sa-zA-Z0-9@!%&-._?]+$/).min(8 , 'حداقل 8 کاراکتر وارد کنید'),
-    birth_date: Yup.string().matches(/^[0-9/\ \s-]+$/ , 'فقط از اعداد و خط تیره استفاده شود'),
+    password: Yup.string().required('لطفا این قسمت را پر کنید').matches(/^[u0600-\u06FF\sa-zA-Z0-9@!%&-._?]+$/).min(8, 'حداقل 8 کاراکتر وارد کنید'),
+    birth_date: Yup.string().matches(/^[0-9/\ \s-]+$/, 'فقط از اعداد و خط تیره استفاده شود'),
     gender: Yup.number(),
-    roles_id: Yup.array().min(1 , 'حد اقل یک مورد را انتخاب کنید'),
+    roles_id: Yup.array().min(1, 'حد اقل یک مورد را انتخاب کنید'),
 })
--- src/services/users.js
+++ src/services/users.js
@@ -1,37 +1,49 @@
 import httpService from "./httpService"
 
-export const getAllPermissionsService = () =>{
-    return httpService('/admin/permissions' , 'get')
+export const getAllPermissionsService = () => {
+    return httpService('/admin/permissions', 'get')
 }
 
-export const getAllRolesService = ()=>{
-    return httpService('/admin/roles' , 'get')
+export const getAllRolesService = () => {
+    return httpService('/admin/roles', 'get')
 }
 
-export const addNewRoleService = (data)=>{
-    return httpService('/admin/roles' , 'post' , data)
+export const addNewRoleService = (data) => {
+    return httpService('/admin/roles', 'post', data)
 }
 
-export const getSingleRoleService = (roleId)=>{
-    return httpService(`/admin/roles/${roleId}` , 'get')
+export const getSingleRoleService = (roleId) => {
+    return httpService(`/admin/roles/${roleId}`, 'get')
 }
 
-export const editRoleService = (roleId , data)=>{
-    return httpService(`/admin/roles/${roleId}` , 'put' , data)
+export const editRoleService = (roleId, data) => {
+    return httpService(`/admin/roles/${roleId}`, 'put', data)
 }
 
-export const editRolePermissionsService = (roleId , data)=>{
-    return httpService(`/admin/roles/${roleId}/permissions` , 'put' , data)
+export const editRolePermissionsService = (roleId, data) => {
+    return httpService(`/admin/roles/${roleId}/permissions`, 'put', data)
 }
 
-export const deleteRoleService = (roleId)=>{
-    return httpService(`/admin/roles/${roleId}` , 'delete')
+export const deleteRoleService = (roleId) => {
+    return httpService(`/admin/roles/${roleId}`, 'delete')
 }
 
-export const getAllUsersService = (page , countOnPage , searchChar)=>{
-    return httpService(`/admin/users?page=${page}&count=${countOnPage}&searchChar=${searchChar}` , 'get')
+export const getAllUsersService = (page, countOnPage, searchChar) => {
+    return httpService(`/admin/users?page=${page}&count=${countOnPage}&searchChar=${searchChar}`, 'get')
 }
 
-export const deleteUserService = (userId) =>{
-    return httpService(`/admin/users/${userId}` , 'delete')
+export const deleteUserService = (userId) => {
+    return httpService(`/admin/users/${userId}`, 'delete')
+}
+
+export const getSingleUserService = (userId) => {
+    return httpService(`/admin/users/${userId}`, 'get')
+}
+
+export const addNewUserService = (data) => {
+    return httpService('/admin/users', 'post', data)
+}
+
+export const editUserSevice = (userId, data) => {
+    return httpService(`/admin/users/${userId}`, 'put', data)
 }
