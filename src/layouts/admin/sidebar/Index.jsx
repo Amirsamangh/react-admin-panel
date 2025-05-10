99cedfbfb412020992c21da9fc90ce588012a4af
AmirSaman Gharechahi <gharechahi72@gmail.com>
2025-05-10 11:41:17 +0330

    react project - permissions 158

--- src/layouts/admin/sidebar/Index.jsx
+++ src/layouts/admin/sidebar/Index.jsx
@@ -18,42 +18,42 @@ const Index = () => {
           imagePath={user.image || "/assets/images/avatar/user6.png"}
           />
 
-          <SidebarItem targetPth='/' title='داشبورد' icon=' fas fa-tachometer-alt'/>
+          <SidebarItem targetPth='/' title='داشبورد' icon=' fas fa-tachometer-alt' pTitle={"read_"}/>
           {/* <!-- =================================== --> */}
 
 
-          <SidebarGroupTitile title='فروشگاه'/>
+          <SidebarGroupTitile title='فروشگاه' pTitles={['read_categories', 'read_products', 'read_colors', 'read_guarantees', 'read_brands', 'read_discounts']}/>
 
 
-          <SidebarItem targetPth='/categories' icon='fas fa-stream' title='مدیریت گروه محصول' />
-          <SidebarItem targetPth='/products' icon='fas fa-cube' title='مدیریت محصول' />
-          <SidebarItem targetPth='/colors' icon='fas fa-palette' title='مدیریت رنگ ها' />
-          <SidebarItem targetPth='/guaranties' icon='fab fa-pagelines' title='مدیریت گارانتی ها' />
-          <SidebarItem targetPth='/brands' icon='fas fa-copyright' title='مدیریت برند ها' />
-          <SidebarItem targetPth='/discounts' icon='fas fa-percentage' title='مدیریت تخفیف ها' />
+          <SidebarItem targetPth='/categories' icon='fas fa-stream' title='مدیریت گروه محصول' pTitle={"read_categories"} />
+          <SidebarItem targetPth='/products' icon='fas fa-cube' title='مدیریت محصول' pTitle={"read_products"} />
+          <SidebarItem targetPth='/colors' icon='fas fa-palette' title='مدیریت رنگ ها' pTitle={"read_colors"} />
+          <SidebarItem targetPth='/guaranties' icon='fab fa-pagelines' title='مدیریت گارانتی ها' pTitle={"read_guarantees"} />
+          <SidebarItem targetPth='/brands' icon='fas fa-copyright' title='مدیریت برند ها' pTitle={"read_brands"} />
+          <SidebarItem targetPth='/discounts' icon='fas fa-percentage' title='مدیریت تخفیف ها' pTitle={"read_discounts"} />
 
           {/* <!-- =================================== --> */}
 
-          <SidebarGroupTitile title='سفارشات و سبد خرید'/>
+          <SidebarGroupTitile title='سفارشات و سبد خرید' pTitles={['read_carts', 'read_orders', 'read_deliveries']}/>
 
-          <SidebarItem targetPth='/carts' icon='fas fa-shopping-basket' title='مدیریت سبد ها' />
-          <SidebarItem targetPth='/orders' icon='fas fa-luggage-cart' title='مدیریت سفارشات' />
-          <SidebarItem targetPth='/deliverires' icon='fas fa-truck-loading' title='مدیریت نحوه ارسال' />
+          <SidebarItem targetPth='/carts' icon='fas fa-shopping-basket' title='مدیریت سبد ها' pTitle={"read_carts"} />
+          <SidebarItem targetPth='/orders' icon='fas fa-luggage-cart' title='مدیریت سفارشات' pTitle={"read_orders"} />
+          <SidebarItem targetPth='/deliverires' icon='fas fa-truck-loading' title='مدیریت نحوه ارسال' pTitle={"read_deliveries"} />
           
           {/* <!-- =================================== --> */}
 
-          <SidebarGroupTitile title='کاربران و همکاران'/>
+          <SidebarGroupTitile title='کاربران و همکاران' pTitles={['read_users', 'read_roles', 'read_permissions']}/>
 
-          <SidebarItem targetPth='/users' icon='fas fa-users' title='مشاهده کاربران' />
-          <SidebarItem targetPth='/roles' icon='fas fa-user-tag' title='نقش ها' />
-          <SidebarItem targetPth='/permissions' icon='fas fa-shield-alt' title='مجوز ها' />
+          <SidebarItem targetPth='/users' icon='fas fa-users' title='مشاهده کاربران' pTitle={"read_users"} />
+          <SidebarItem targetPth='/roles' icon='fas fa-user-tag' title='نقش ها' pTitle={"read_roles"} />
+          <SidebarItem targetPth='/permissions' icon='fas fa-shield-alt' title='مجوز ها' pTitle={"read_permissions"} />
           
           {/* <!-- =================================== --> */}
 
-          <SidebarGroupTitile title='ارتباطات'/>
+          <SidebarGroupTitile title='ارتباطات' pTitles={['read_questions', 'read_comments']}/>
 
-          <SidebarItem targetPth='/questions' icon='fas fa-question-circle' title='سوال ها' />
-          <SidebarItem targetPth='/comments' icon='fas fa-comment' title='نظرات' />
+          <SidebarItem targetPth='/questions' icon='fas fa-question-circle' title='سوال ها' pTitle={"read_questions"} />
+          <SidebarItem targetPth='/comments' icon='fas fa-comment' title='نظرات' pTitle={"read_comments"} />
 
         </div>
       </div>
--- src/layouts/admin/sidebar/SidebarGroupTitile.jsx
+++ src/layouts/admin/sidebar/SidebarGroupTitile.jsx
@@ -1,12 +1,17 @@
 
 import React from "react";
+import { useHasPermission } from "../../../hook/permissionHook";
 
-const SidebarGroupTitle = ({ title }) => {
+const SidebarGroupTitle = ({ title, pTitles }) => {
+  const hasPerm = useHasPermission(pTitles)
   return (
-    <div className="py-1 text-start d-flex justify-content-center no_pointer no_hover siebar_items">
-      <span className="hiddenable no_wrap group_sidebar_title">{title}</span>
-    </div>
-  );
+    hasPerm && (
+      <div className="py-1 text-start d-flex justify-content-center no_pointer no_hover siebar_items">
+        <span className="hiddenable no_wrap group_sidebar_title">{title}</span>
+      </div>
+    )
+  )
+
 };
 
 export default SidebarGroupTitle;
