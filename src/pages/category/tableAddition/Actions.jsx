941a890621ab7fb8b7c12670e6e4877632337707
Amirsaman <amirsaman_gh@live.com>
2025-05-11 12:34:59 +0330

    react project - permissions 159

--- src/pages/category/tableAddition/Actions.jsx
+++ src/pages/category/tableAddition/Actions.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { CategoryContext } from '../../../context/categoryContext';
+import ActionIcon from '../../../components/ActionIcon';
 
 const Actions = ({ rowData, handleDeleteCategory }) => {
 
@@ -12,52 +13,31 @@ const Actions = ({ rowData, handleDeleteCategory }) => {
         <>
 
             {!params.categoryId ? (
-                <i
-                    className="fas fa-project-diagram text-info mx-1 hoverable_text pointer has_tooltip"
-                    title="زیرمجموعه"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    onClick={() => {
-                        navigate(`/categories/${rowData.id}`, {
-                            state: {
-                                parentData: rowData
-                            }
-                        })
-                    }}
-                ></i>
+                <ActionIcon icon="fas fa-project-diagram text-info" pTitle="زیرمجموعه" data-bs-toggle="tooltip" data-bs-placement="top" onClick={() => {
+                    navigate(`/categories/${rowData.id}`, {
+                        state: {
+                            parentData: rowData
+                        }
+                    })
+                }}
+                />
             ) : null}
 
-            <i
-                className="fas fa-edit text-warning mx-1 hoverable_text pointer has_tooltip"
-                title="ویرایش دسته"
-                data-bs-toggle="modal"
-                data-bs-placement="top"
-                data-bs-target='#add_product_category_modal'
+            <ActionIcon icon="fas fa-edit text-warning" pTitle="ویرایش دسته" data-bs-toggle="modal" data-bs-placement="top" data-bs-target='#add_product_category_modal'
                 onClick={() => setEditId(rowData.id)}
-            ></i>
+            />
 
             {params.categoryId ? (
-                <i
-                    className="fas fa-receipt text-success mx-1 hoverable_text pointer has_tooltip"
-                    title="افزودن ویژگی"
-                    data-bs-placement="top"
-                    onClick={() => {
-                        navigate(`/categories/${rowData.id}/attrbutes`, {
-                            state: {
-                                categoryData: rowData
-                            }
-                        })
-                    }}
-                ></i>
+                <ActionIcon icon="fas fa-receipt text-success" pTitle="افزودن ویژگی" onClick={() => {
+                    navigate(`/categories/${rowData.id}/attrbutes`, {
+                        state: {
+                            categoryData: rowData
+                        }
+                    })
+                }}
+                />
             ) : null}
-
-            <i
-                className="fas fa-times text-danger mx-1 hoverable_text pointer has_tooltip"
-                title="حذف دسته"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-                onClick={() => handleDeleteCategory(rowData)}
-            ></i>
+            <ActionIcon icon="fas fa-times text-danger" pTitle="حذف دسته" onClick={() => handleDeleteCategory(rowData)} />
         </>
     );
 }
